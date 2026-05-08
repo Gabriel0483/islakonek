2ac815ad2ed827156f91e8ce6780ee4b68b25b42
Gabriel Magpantay <rielmagpantay@gmail.com>
2026-05-08 02:32:45 +0000

    check firebase config for error

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+// Your web app's Firebase configuration
 export const firebaseConfig = {
-  "projectId": "studio-6954150183-9f417",
-  "appId": "1:331769284679:web:1cc10030909e453b4b0962",
-  "apiKey": "AIzaSyDb_j2KiCAajXvacCpbtOVR-jlZmU-LKZ4",
-  "authDomain": "studio-6954150183-9f417.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "331769284679"
+  apiKey: "AIzaSyDb_j2KiCAajXvacCpbtOVR-jlZmU-LKZ4",
+  authDomain: "studio-6954150183-9f417.firebaseapp.com",
+  databaseURL: "https://studio-6954150183-9f417-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "studio-6954150183-9f417",
+  storageBucket: "studio-6954150183-9f417.firebasestorage.app",
+  messagingSenderId: "331769284679",
+  appId: "1:331769284679:web:1cc10030909e453b4b0962"
 };
