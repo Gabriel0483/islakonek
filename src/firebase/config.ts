161a0f37d6715f9518f8ba7b0ce2a0a3b4d273bf
Gabriel Magpantay <rielmagpantay@gmail.com>
2026-05-12 22:10:12 +0000

    check firebase config for errors

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,19 @@
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyDb_j2KiCAajXvacCpbtOVR-jlZmU-LKZ4",
-  authDomain: "studio-6954150183-9f417.firebaseapp.com",
-  databaseURL: "https://studio-6954150183-9f417-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "studio-6954150183-9f417",
-  storageBucket: "studio-6954150183-9f417.firebasestorage.app",
-  messagingSenderId: "331769284679",
-  appId: "1:331769284679:web:1cc10030909e453b4b0962"
+  apiKey:"AIzaSyDb_j2KiCAajXvacCpbtOVR-jlZmU-LKZ4",
+  authDomain:"studio-6954150183-9f417.firebaseapp.com",
+  databaseURL:"https://studio-6954150183-9f417-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId:"studio-6954150183-9f417",
+  storageBucket:"studio-6954150183-9f417.firebasestorage.app",
+  messagingSenderId:"331769284679",
+  appId:"1:331769284679:web:816648879aded5004b0962"
 };
+
+export const isFirebaseConfigValid = Boolean(
+  firebaseConfig.apiKey && 
+  firebaseConfig.apiKey !== "YOUR_API_KEY" &&
+  firebaseConfig.projectId &&
+  firebaseConfig.projectId !== "YOUR_PROJECT_ID"
+);
+
+
